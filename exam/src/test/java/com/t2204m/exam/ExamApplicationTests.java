27a601a0846d02a834bdbe2ffdd8ed1f4794b4be
Nguyen Duy Quoc <nguyenduyquoc129829042001@gmail.com>
2024-02-20 10:54:02 +0700

    first_commit

---
+++ exam/src/test/java/com/t2204m/exam/ExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t2204m.exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
