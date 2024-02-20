798c904022bfabc9b6609bd3d23934347a763158
pokabi11 <hlong2k1@gmail.com>
2024-02-20 16:55:15 +0700

    a

---
+++ src/main/java/com/t2207e/wcd_test/WcdTestApplication.java
@@ -0,0 +1,13 @@
+package com.t2207e.wcd_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WcdTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WcdTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/t2207e/wcd_test/WcdTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t2207e.wcd_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WcdTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
