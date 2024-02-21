1f3e7daabbe8c6daaeedc52c91b9a3e3643e8527
Admin <anhltth2203042@fpt.edu.vn>
2024-02-21 18:05:22 +0700

    Firt Commit

---
+++ exam/src/test/java/com/t2207e/exam/ExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t2207e.exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
